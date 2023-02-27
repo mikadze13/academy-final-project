00394896624b1343ab4e19ce7fbc9272fd0c2a9a
vazha mikadze <vazhamika@gmail.com>
2023-02-27 22:45:51 +0400

    update environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   baseUrl:'https://user-318ab-default-rtdb.europe-west1.firebasedatabase.app/'
 };
 

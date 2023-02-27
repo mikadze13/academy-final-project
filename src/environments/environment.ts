2c3d4e8e2e1b3848ed7e6d821ee3186a24d9e616
vazha mikadze <vazhamika@gmail.com>
2023-02-27 23:03:26 +0400

    Update environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,8 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment:any = {
-  production: false,
+export const environment = {
+  production: true,
   baseUrl:'https://user-318ab-default-rtdb.europe-west1.firebasedatabase.app/'
 };
 
